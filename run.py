ec70f69f40793e6d2c891c1aec8bc5e33660077c
JoseCarlosVJr <josejunior.vieira@hotmail.com>
2019-12-01 12:39:34 -0300

    concluido

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
-from services.origin_to_phone import IdentifyOriginPhone
+# import speech_recognition as sr
+# import soundfile as sf
+#
+# file_audio_ogg = 'database/audio_brenda.ogg'
+# file_audio_wav = 'database/out.wav'
+# r = sr.Recognizer()
+# with sr.AudioFile(file_audio_wav) as source:
+#     audio = r.listen(source)
+#
+# print(r.recognize_google(audio, language='pt-BR'))
+from datetime import date
 
-request = IdentifyOriginPhone('+55 48 9 9***-****')
-print(request)
+LOCAL = 'database/tot.txt'
+A = 1574879999
+B = 1575249999
+
+timestamp = date.fromtimestamp(A)
+timestamp2 = date.fromtimestamp(B)
+
+with open(LOCAL, 'w') as r:
+    for i in range(A, B, 50):
+        r.writelines(str(i)+'\n')
+r.close()
+print("Date =", timestamp, B - A)
